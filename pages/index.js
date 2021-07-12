4331aac432f346a4b844a64363c421af31bf31b6
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-07-12 20:46:58 -0300

    Day one

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        alt="Profile"
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
+
+export default function Home() {
+  const githubUser = "dudunog";
+  const favoritePeople = ["peas", "rafaballerini"];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({favoritePeople.length})
+            </h2>
+            <ul>
+              {favoritePeople.map(currentValue => {
+                return (
+                  <li>
+                    <a href={`/users/${currentValue}`} key={currentValue}>
+                      <img
+                        src={`https://github.com/${currentValue}.png`}
+                        alt={currentValue}
+                      />
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
+}
